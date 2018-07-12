03f51e92bbafe2b6f75eb7ca815c9bf9bb9af48e
Lawrence Kirk <lmkirkiii@gmail.com>
2018-07-12 15:48:22 -0400

    added in a lot of css styles and text information to create a more well rounded page

--- js/style.js
+++ js/style.js
@@ -1,10 +1,11 @@
 const image = document.querySelector('.front');
 var questionsArray= ['True or false do animals that Lay eggs have belly buttons?','Who was the first President born a US citizen?','Which country privately owns the most tigers world wide?','Do Tomatoes have more genes then humans?','How long does the largest mushroom in the world strech for?','What is the word to describe a flock of crows?']
 var question = []
-var answersArray = ['No of course they dont!','Martin Van Buren!','The U.S.A!','Yes they do! Saucey!','3.5 miles!','A  murder!']
+var answersArray = ['No of course they dont!','Martin Van Buren!','The U S A!','Yes they do! Saucey!','3.5 miles!','A  murder!']
 var answer = [];
 var incorrectAnswerArray = [];
 var incorrectQuestionArray = [];
+var card = document.querySelector('.card')
 var hideQ = document.querySelector('#question')
 var showQ = document.querySelector('#question')
 var hideA = document.querySelector('#answer')
@@ -12,15 +13,29 @@ var showA = document.querySelector('#answer')
 var reviewshow = document.querySelector('#answerReview')
 var backCap1 = document.querySelector('.back1')
 var backCap = document.querySelector('.back')
+var reviewGame = document.querySelector('#game-over-review')
 var gameOver = document.querySelector('#game-over')
-var correctBtn = document.querySelector('.button1')
-var incorrectBtn = document.querySelector('.button2')
-var reviewBtn = document.querySelector('.button3')
+var correctBtn = document.querySelector('#button1')
+var incorrectBtn = document.querySelector('#button2')
+var reviewBtn = document.querySelector('#button3')
 var cardClass = document.querySelector('.card')
 var reviewClass = document.querySelector('.review')
+var historyClass = document.querySelector('.history')
 var reviewFront = document.querySelector('.front1')
 var reviewBack = document.querySelector('.reviewback1')
 var reviewBack2 = document.querySelector('.reviewBack2')
+var redbutton =document.querySelector('.red')
+var orangebutton = document.querySelector('.orange')
+var lightgreenbutton = document.querySelector('.yellow')
+var greenbutton = document.querySelector('.green')
+var developers = document.querySelector('.developers')
+var theorytext = document.querySelector('.theory')
+var rules = document.querySelector('.rules')
+var buttonsWhole = document.querySelector('.button-container')
+
+
+//First half of Flash cards. 
+
 image.addEventListener("click",function(evt){
     for(i = 0; i < 1; i++){
         question.push(questionsArray[i])
@@ -81,24 +96,24 @@ incorrectBtn.addEventListener("click",function(evt){
 reviewBtn.addEventListener("click",function(){
     cardClass.style.display = 'none';
     reviewClass.style.display = 'block';
+    buttonsWhole.style.display = 'none'
 
 })
 
 reviewFront.addEventListener("click",function(evt){
     for(i = 0; i < 1; i++){
         question.push(incorrectQuestionArray[i])
-        incorrectQuestionArray.shift()
+       
         if (incorrectQuestionArray.length === 0 ){
-            cardClass.style.display = 'block';
-            reviewClass.style.display = 'none';
+            reviewGame.style.display = 'inline' 
         }
-        
-      }
+        incorrectQuestionArray.shift();
+    }
        showQ.style.display = 'inline'
        reviewFront.style.display = 'none'
        reviewBack.style.display = 'inline'
        question.toString();
-       document.querySelector('#question').innerHTML = question; 
+       document.querySelector('#question').innerText = question; 
        console.log(evt)
    })
 
@@ -108,18 +123,17 @@ reviewBack.addEventListener("click", function(event){
     event.preventDefault()
     for(i = 0; i < 1; i++){
         answer.push(incorrectAnswerArray[i])
-        incorrectAnswerArray.shift()
         if (incorrectAnswerArray.length === 0 ){
-            cardClass.style.display = 'block';
-            reviewClass.style.display = 'none';
+            reviewGame.style.display = 'inline' 
         }
-    }
+        incorrectAnswerArray.shift()
+        }
     reviewshow.style.display = 'inline'
     hideQ.style.display = 'none'
     reviewBack.style.display = 'none'
     reviewBack2.style.display = 'inline'
     answer.toString();
-    document.querySelector('#answerReview').innerHTML = answer; 
+    document.querySelector('#answerReview').innerText = answer; 
 })
 
 reviewBack2.addEventListener("click",function(evt){
@@ -128,4 +142,67 @@ reviewBack2.addEventListener("click",function(evt){
     reviewFront.style.display = 'inline'
     reviewshow.style.display ='none'
     gameOver.style.display = 'none'
+})
+
+// Page content :
+
+redbutton.addEventListener('click',function(event){
+    event.preventDefault();
+    theorytext.style.display = 'block'
+    developers.style.display = 'none'
+    rules.style.display = 'none'
+    historyClass.style.display = 'none'
+    card.style.display = 'none'
+    buttonsWhole.style.display = 'none'
+    review.style.display = 'none'
+    buttonsWhole.style.display = 'none'
+
+})
+
+orangebutton.addEventListener('click',function(event){
+    event.preventDefault();
+    historyClass.style.display = 'block'
+    developers.style.display = 'none'
+    rules.style.display = 'none'
+    theorytext.style.display = 'none'
+    card.style.display = 'none'
+    buttonsWhole.style.display = 'none'
+    review.style.display = 'none'
+    buttonsWhole.style.display = 'none'
+    
+
+})
+
+orangebutton.addEventListener('click',function(event){
+    event.preventDefault();
+    historyClass.style.display = 'block'
+    developers.style.display = 'none'
+    rules.style.display = 'none'
+    theorytext.style.display = 'none'
+    buttonsWhole.style.display = 'none'
+    card.style.display = 'none'
+    review.style.display = 'none'
+    
+})
+
+lightgreenbutton.addEventListener('click',function(event){
+    event.preventDefault();
+    rules.style.display = 'block'
+    developers.style.display = 'none'
+    historyClass.style.display = 'none'
+    theorytext.style.display = 'none'
+    buttonsWhole.style.display = 'none'
+    card.style.display = 'none'
+    review.style.display = 'none'
+})
+greenbutton.addEventListener('click',function(event){
+    event.preventDefault();
+    developers.style.display = 'block'
+    rules.style.display = 'none'
+    historyClass.style.display = 'none'
+    theorytext.style.display = 'none'
+    buttonsWhole.style.display = 'none'
+    card.style.display = 'none'
+    review.style.display = 'none'
+    
 })
